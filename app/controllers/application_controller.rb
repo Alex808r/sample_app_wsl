1042a999aadb976c8fd62cd7bbbc544640532ed9
Alex808r <fromxlam@gmail.com>
2021-06-21 21:20:06 +0300

    Add assert select to the test static pages controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < ActionController::Base
-
+=begin
+использовали перед первым деплоем на хероку для проверки
+в 3 главе удаляем.
   def hello
   render html: "Hello, this is Ruby on Rails Tutorial"
   end
+=end
+
 
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,32 @@
 require "test_helper"
 
+#assert_select позволяет нам тестировать наличие определённого тэга HTML
+# (их также называют “селекторами”, отсюда и assert_select)
+# Этот код проверяет наличие тэга <title>, содержащего строку “Home | Ruby on Rails Tutorial Sample App”
+
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    #
+    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+
+    #
+    assert_select "h1", "Sample App"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
+    #
+    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
   end
 
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+    #
+    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
   end
 
 end
-
-
