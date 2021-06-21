4e37a746893aaf2e0c9d3ad5d3bd6bdeb42cc809
Alex808r <fromxlam@gmail.com>
2021-06-21 16:52:10 +0300

    Add test to the Static Pages Controller and add the page About

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
-  #def hello
-  # render html: "Hello, this is Ruby on Rails Tutorial"
-  #end
+  def hello
+  render html: "Hello, this is Ruby on Rails Tutorial"
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/home'
   get 'static_pages/help'
+  get 'static_pages/about'
   #root 'static_pages#home'
 
-  #root 'application#hello'
+  root 'application#hello'
 end
