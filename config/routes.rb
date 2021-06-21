866528f219dbf88aa0b83fd51a9dee3f2c86ef25
Alex808r <fromxlam@gmail.com>
2021-06-21 21:57:52 +0300

    Change routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root 'static_pages#home'
+  # get 'static_pages/home' - Обратите внимание, что теперь предыдущий маршрут static_pages/home больше не будет работать
+  # так как он определен корневым маршуртом при помощи root
 
-  get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
 
