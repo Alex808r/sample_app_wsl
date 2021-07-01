52a3338d0c8161ba14d64aa6617ced9bb0952512
Alex808r <fromxlam@gmail.com>
2021-07-01 16:06:11 +0300

    Add regex to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   before_save { email.downcase! }
   validates :name,  presence: true, length: { maximum: 50 } #валидация наличия имени и его длинны не более 50
 
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  #VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i это выражение пропусить емайл с двумя точками  foo@bar..com, следующее выражение не пропустит
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },  uniqueness: { case_sensitive: false }
   # валидация наличия емайл и его длины не более 255 и соответстия требованиям емайла и              уникальности   чувствительности к регистру
 
