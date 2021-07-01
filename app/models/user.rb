6c81bb42eff02fd092ba858dc76219c0e0e30fa4
Alex808r <fromxlam@gmail.com>
2021-07-01 14:46:15 +0300

    Add test to save email downcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase! } # перевод емайл в нижний регистр перед сохранением в базу данных
   validates :name,  presence: true, length: { maximum: 50 } #валидация наличия имени и его длинны не более 50
-  validates :email, presence: true, length: { maximum: 255 } # валидация наличия емайл и его длины не более 255
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX },  uniqueness: { case_sensitive: false }
+  # валидация наличия емайл и его длины не более 255 и соответстия требованиям емайла и              уникальности   чувствительности к регистру
 end
