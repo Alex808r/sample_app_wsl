b20ef6b1e6457f07ae63c43439ef1f530d95dbfb
Alex808r <fromxlam@gmail.com>
2021-07-01 10:48:32 +0300

    Add to integration test test helper

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  #  Первый интеграционный тест. Функции:
+  # Первый интеграционный тест глава 5. Функции:
   # Посетить корневой путь (Home-страница)
   # Убедиться, что обрабатывается правильный шаблон страницы
   # Проверить корректность ссылок на страницы Home, Help, About и Contact
@@ -13,5 +13,21 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", signup_path # проверяем проход по кнопке регистрации
   end
+
+  # добавьте код для посещения страницы регистрации с помощью
+  # метода get и убедитесь, что заголовок полученной страницы является верным
+
+  def setup
+    @base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+  test "title signup page" do
+    get signup_path
+      #assert_select "title", "Sign up | #{@base_title}"
+      # или так с использованием хелпера full_title("Sign up")
+    assert_select "title", full_title("Sign up")
+  end
+
 end
