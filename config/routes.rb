803daa6c2301ede54281c7eee2397a17f986b507
Alex808r <fromxlam@gmail.com>
2021-07-02 15:48:34 +0300

    Add form signup and integration test to form

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
-  get 'users/new'
+
   root 'static_pages#home'
   #get 'static_pages/home'
   # - Обратите внимание, что теперь предыдущий маршрут static_pages/home больше не будет работать
   # так как он определен корневым маршуртом при помощи root
 
-  #get 'static_pages/help'
-  get '/help',    to: 'static_pages#help'
+
+  get '/help',    to: 'static_pages#help'    #get 'static_pages/help' было при создании контроллера
   get '/about',   to: 'static_pages#about'   #get 'static_pages/about'
   get '/contact', to: 'static_pages#contact' #get 'static_pages/contact'
   get '/signup',  to: 'users#new'
 
+  #
+  #get 'users/new'
   resources :users
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,30 +1,37 @@
 require "test_helper"
 
-#assert_select позволяет нам тестировать наличие определённого тэга HTML
-# (их также называют “селекторами”, отсюда и assert_select)
-# Этот код проверяет наличие тэга <title>, содержащего строку “Home | Ruby on Rails Tutorial Sample App”
+  # assert_select позволяет нам тестировать наличие определённого тега HTML
+  # их также называют "селекторами", отсюда и "assert_select"
+  # Тест "should get home": протестируем страницу Home, выдав запрос GET к действию home, и затем убедившись,
+  # что в ответ мы получаем код состояния ‘success (успех)’.”
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
+  # перед выполнением теста переменной "@base_title" пристваивается значение
+  # setup, автоматически запускается перед каждым тестом
   def setup
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
   test "should get home" do
+    # пройти по корневому пути (root_path) и проверить успешный get запрос или нет
     get root_path
-    #проверить успешный get запрос или нет.
     assert_response :success
-    #
-    #assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+
+    # Этот код проверяет наличие тега <title>, содержащего строку "Ruby on Rails Tutorial Sample App".
+    # Можно записать 2 способами: с использованием переменной "@base_title" и без нее
+    # assert_select "title", "Ruby on Rails Tutorial Sample App"
+    # Хелпер заголовка для страницы "home" не используется, для других статических страниц используется
     assert_select "title", "#{@base_title}"
     #
     assert_select "h1", "Welcome to the Sample App"
   end
 
   test "should get help" do
+    # пройти по help пути и проверить успешный get запрос или нет
     get help_path
     assert_response :success
-    #
+
     #assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
     assert_select "title", "Help | #{@base_title}"
   end
@@ -32,7 +39,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get about" do
     get about_path
     assert_response :success
-    #
+
     #assert_select "title", "About | Ruby on Rails Tutorial Sample App"
     assert_select "title", "About | #{@base_title}"
   end
@@ -40,9 +47,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get contact" do
     get contact_path
     assert_response :success
-    #
-    assert_select "title", "Contact | #{@base_title}"
 
+    #assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contact | #{@base_title}"
   end
 
 end
